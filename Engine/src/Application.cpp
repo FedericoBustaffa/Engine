cbf6950e26e23b098a4f443ce5ff55b9109fdb86
Busti88 <kiko.bustaffa@gmail.com>
2020-10-22 16:43:15 +0200

    test

--- Engine/src/Application.cpp
+++ Engine/src/Application.cpp
@@ -15,20 +15,18 @@ Application::Application()
 	glGenVertexArrays(1, &va);
 	glBindVertexArray(va);
 
-	glGenBuffers(1, &vb);
-	glBindBuffer(GL_ARRAY_BUFFER, vb);
 	float vertices[3 * 3] =	{
 		-0.5f, -0.5f, 0.0f,
 		 0.0f,  0.5f, 0.0f,
 		 0.5f, -0.5f, 0.0f
 	};
+	glGenBuffers(1, &vb);
+	glBindBuffer(GL_ARRAY_BUFFER, vb);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+	unsigned int indices[3] = { 0, 1, 2 };
 	glGenBuffers(1, &ib);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ib);
-	unsigned int indices[3] = {
-		0, 1, 2
-	};
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
