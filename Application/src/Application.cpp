407e0d7250d74d9dd78bb7a513180573cb8282e5
Busti88 <kiko.bustaffa@gmail.com>
2020-11-01 12:51:28 +0100

    transform

--- Application/src/Application.cpp
+++ Application/src/Application.cpp
@@ -19,12 +19,12 @@ Application::Application()
 
 		layout(location = 0) in vec4 v_position;
 	
-		uniform mat4 view_projection;
+		uniform mat4 view;
 		uniform mat4 transform;
 	
 		void main()
 		{
-			gl_Position = /*transform */ v_position;
+			gl_Position = view * transform * v_position;
 		}
 	)";
 
@@ -35,24 +35,30 @@ Application::Application()
 
 		void main()
 		{
-			color = vec4(0.4, 0.3, 0.1, 1.0);
+			color = vec4(0.4, 0.75, 0.2, 1.0);
 		}
 	)";
 
 	shader = std::make_shared<Shader>(vertex_src, fragment_src);
+	glm::mat4 view = glm::ortho(-1.0, 1.0, -1.0, 1.0);
+	shader->SetUniformMat4("view", view);
 
 	// buffers
-	double vertices[3 * 2] = {
+	double vertices[4 * 2] = {
 		-0.5, -0.5,
-		 0.0,  0.5,
+		-0.5,  0.5,
+		 0.5,  0.5,
 		 0.5, -0.5
 	};
 	
-	uint32_t indices[3] = { 0, 1, 2 };
+	uint32_t indices[6] = {
+		0, 1, 2,
+		2, 3, 0
+	};
 	
 	va = std::make_shared<VertexArray>();
 	vb = std::make_shared<Buffer>(sizeof(vertices), vertices);
-	ib = std::make_shared<IndexBuffer>(3, indices);
+	ib = std::make_shared<IndexBuffer>(6, indices);
 
 	std::shared_ptr<Layout> layout;
 	layout.reset(new Layout({
@@ -70,6 +76,8 @@ Application::~Application()
 
 void Application::Run()
 {
+	glm::vec3 position(0.0f);
+	
 	while (window.IsOpen())
 	{
 		// timestep
@@ -78,13 +86,29 @@ void Application::Run()
 		// background color
 		Render::BackgroundColor(0.07f, 0.07f, 0.07f, 1.0f);
 
+		
 		// rendering
 		Render::BeginScene();
 
+		if (Input::IsKeyPressed(window, Key::A))
+			position.x -= 5.0f * ts.GetSeconds();
+		else if (Input::IsKeyPressed(window, Key::D))
+			position.x += 5.0f * ts.GetSeconds();
+		
+		if (Input::IsKeyPressed(window, Key::W))
+			position.y += 5.0f * ts.GetSeconds();
+		else if (Input::IsKeyPressed(window, Key::S))
+			position.y -= 5.0f * ts.GetSeconds();
+		
+		
+		glm::mat4 transform = glm::translate(glm::mat4(1.0f), position);
+
+		shader->SetUniformMat4("transform", transform);
 		shader->Bind();
 		Render::DrawIndexed(va);
 
 		Render::EndScene();
+
 
 		// event polling
 		window.OnUpdate();
